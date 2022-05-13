4a9ca574544105c078c6d40d47a7d2936f2050be
MwangiGregory <gmwaburi@gmail.com>
2022-05-13 08:36:47 +0000

    Add prompt functionality
    Used _putchar to display prompt text
    to the user.

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "shell.h"
+
+/**
+ * main - entry point
+ * Return: Always zero
+ */
+int main(void)
+{
+	char *prompt_text = "$";
+
+	_putchar(prompt_text);
+	return (0);
+}
