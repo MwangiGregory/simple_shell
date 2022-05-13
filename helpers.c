a2d1dfbfcefe24eb5b0a030052f877cd05fdc042
MwangiGregory <gmwaburi@gmail.com>
2022-05-13 12:23:00 +0000

    Define function split_string

--- helpers.c
+++ helpers.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -15,5 +16,26 @@ void _putchar(char *s)
 	{
 		perror("Write failed to output to stdout");
 		return;
+	}
+}
+/**
+ * split_string - splits a string into 1 or more non empty tokens.
+ * @line: pointer to a string
+ * @strings: array of pointers
+ * @delim: pointer to delimiting character/string
+ */
+void split_string(char *line, char **strings, char *delim)
+{
+	char *token;
+	int i = 0;
+
+	token = strtok(line, delim);
+	strings[i] = token;
+	i++;
+	while (token)
+	{
+		token = strtok(NULL, delim);
+		strings[i] = token;
+		i++;
 	}
 }
