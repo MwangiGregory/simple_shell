bf6461310ed7582ae87f1809d468e0deb48f1fca
MwangiGregory <gmwaburi@gmail.com>
2022-05-13 12:55:08 +0000

    Allocate memory for an array of pointers

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include "shell.h"
 
+int count_tokens(char *buf);
+
 /**
  * main - entry point
  * Return: Always zero
@@ -12,7 +14,8 @@ int main(void)
 	char *prompt_text = "$";
 	char *buf = NULL;
 	size_t inp_size = 0;
-
+	char **strings;
+	(void)strings;
 	/*infinte loop as long as prompt_text is valid*/
 	while (prompt_text)
 	{
@@ -22,8 +25,29 @@ int main(void)
 			free(buf);
 			perror("\nFailure to read line: ");
 		}
-		else
-			_putchar(buf);
+
+		printf("%d\n",count_tokens(buf));
+		
 	}
 	return (0);
+}
+/**
+ * count_strings - finds the number of words/token in string
+ * pointed to by buf
+ * @buf: pointer to a string
+ * Return: number of tokens in string buf
+ */
+int count_tokens(char *buf)
+{
+	int i = 0;
+	int no_of_tokens = 1;/*min no. of strings will always be 1*/
+	
+	while (buf[i])
+	{
+		if (buf[i] == ' ')
+			no_of_tokens++;
+
+		i++;
+	}
+	return (no_of_tokens);
 }
