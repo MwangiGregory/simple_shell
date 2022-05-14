b75d717270b921824f3800435fddc413dff8c3ea
MwangiGregory <gmwaburi@gmail.com>
2022-05-14 06:48:04 +0000

    Enable hsh to execute other programs
    Used fork to create a child process
    Then used execve to execute another
    process specified by the user.

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,7 +14,8 @@ int main(void)
 	char *buf = NULL;
 	size_t inp_size = 0;
 	char **tokens;
-	int i;
+	int i, status;
+	pid_t my_pid;
 
 	/*infinte loop as long as prompt_text is valid*/
 	while (prompt_text)
@@ -22,12 +24,37 @@ int main(void)
 		if (getline(&buf, &inp_size, stdin) == -1)
 		{
 			free(buf);
-			perror("\nFailure to read line: ");
+			perror("Failure to read line");
+		}
+		/*change newline char to null char*/
+		for (i = 0; buf[i]; i++)
+		{
+			if (buf[i] == '\n')
+				buf[i] = '\0';
 		}
 
 		/*TODO: free this memory*/
 		tokens = malloc(count_tokens(buf) * sizeof(char *));
 		split_string(buf, tokens, " ");
+
+		my_pid = fork();
+		if (my_pid == -1)
+		{
+			perror("Unable to create child process");
+			continue;
+		}
+		if (my_pid == 0)
+		{
+			if (execve(tokens[0], tokens, NULL) == -1)
+			{
+				perror("Unable to execute");
+				_exit(0);
+			}
+			_exit(0);
+		}
+		else
+			wait(&status);
+		free(tokens);
 	}
 	return (0);
 }
