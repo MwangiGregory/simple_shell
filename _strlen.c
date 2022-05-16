9ff2a86d1449d264cb145e801e51208a33f4aacb
MwangiGregory <gmwaburi@gmail.com>
2022-05-16 21:09:21 +0000

    Define function _strlen

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to a string
+ *
+ * Return: Length of string pointed to by s. Length excludes
+ * the NULL character
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
