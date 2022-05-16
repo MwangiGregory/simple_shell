10487aa194d59fd3b633044d57ac2a9e4526cd6c
MwangiGregory <gmwaburi@gmail.com>
2022-05-16 21:06:30 +0000

    Define function _strcpy

---
+++ _strcpy.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including terminating null byte(\0),
+ * to the buffer pointed to by dest.
+ * @src: pointer to string to be copied
+ * @dest: pointer to buffer
+ *
+ * Return: Null if failed, pointer to buffer otherwise.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	if (!dest || !src)
+		return (NULL);
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
