c6cdceb69cd9ff917f61f6713cac83596106bb92
MwangiGregory <gmwaburi@gmail.com>
2022-05-16 17:55:03 +0000

    Define split_string in its own file

---
+++ split_string.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * split_string - splits a string into 1 or more non empty tokens.
+ * @line: pointer to a string
+ * @strings: array of pointers
+ * @delim: pointer to delimiting character/string
+ */
+void split_string(char *line, char **strings, char *delim)
+{
+	char *token;
+	int i = 0;
+
+	token = strtok(line, delim);
+	strings[i] = token;
+	i++;
+	while (token)
+	{
+		token = strtok(NULL, delim);
+		strings[i] = token;
+		i++;
+	}
+}
