6167a72ad31880c625c1e2022af13610f6de2636
MwangiGregory <gmwaburi@gmail.com>
2022-05-16 17:49:38 +0000

    Define count_tokens in its own file

---
+++ count_tokens.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * count_strings - finds the number of words/token in string
+ * pointed to by buf
+ * @buf: pointer to a string
+ * Return: number of tokens in string buf
+ */
+int count_tokens(char *buf)
+{
+	int i = 0;
+	int no_of_tokens = 1;/*min no. of strings will always be 1*/
+
+	while (buf[i])
+	{
+		if (buf[i] == ' ')
+			no_of_tokens++;
+		i++;
+	}
+	return (no_of_tokens);
+}
