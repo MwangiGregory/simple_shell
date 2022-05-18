354aa208a610d8f196ef11e5650882e8cf024e10
MwangiGregory <gmwaburi@gmail.com>
2022-05-18 21:36:40 +0000

    Add function declarations and header files

--- shell.h
+++ shell.h
@@ -5,6 +5,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <sys/wait.h>
 
 void _putchar(char *s);
 void split_string(char *line, char **strings, char *delim);
@@ -13,4 +17,8 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 int count_dirs(char *path);
 char **create_arr_of_dirs(char *path);
+char **command_search(char *command, char **dirs);
+char *_strcat(char *dest, char *src);
+char *_getenv(char *name);
+void execute_command(char *cmd_path, char **arr);
 #endif
